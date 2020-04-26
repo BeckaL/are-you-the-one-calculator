91857cac436327db6f1703d8339fceb71fa613a5
Rebecca Lelew <rebecca.lelew@springernature.com>
2020-04-26 15:27:31 +0100

    Revert "Add couples deriver"
    This reverts commit 077f0ff2

--- test/unit/test_couples_deriver.py
+++ test/unit/test_couples_deriver.py
@@ -30,29 +30,3 @@ def test_derives_matches_from_a_weekly_guess():
     deriver = CouplesDeriver(weekly_guesses, confirmed_matches, confirmed_no_matches)
     assert(deriver.derive() == {"derived_matches": {('G', 'H'), ('I', 'J')}, "derived_no_matches": set()})
 
-def test_derives_matches_across_multiple_weeks():
-    weekly_guesses = [([('A', 'B'), ('C', 'D'), ('E', 'F'), ('G', 'H'), ('I', 'J')], 2),
-                      ([('A', 'E'), ('C', 'D'), ('B', 'H'), ('G', 'J'), ('I', 'F')], 2)]
-    confirmed_no_matches = [('A', 'B'), ('E', 'F'), ('G', 'H'), ('G', 'J'), ('I', 'F'), ('B', 'H')]
-    confirmed_matches = [('C', 'D')]
-    deriver = CouplesDeriver(weekly_guesses, confirmed_matches, confirmed_no_matches)
-    assert(deriver.derive() == {"derived_matches": {('I', 'J'), ('A', 'E')}, "derived_no_matches": set()})
-
-def test_derives_matches_across_multiple_week_using_derived_information():
-    weekly_guesses = [([('A', 'B'), ('C', 'D'), ('E', 'F'), ('G', 'H'), ('I', 'J')], 3),
-                      ([('A', 'E'), ('C', 'D'), ('B', 'F'), ('G', 'H'), ('I', 'J')], 3)]
-    confirmed_no_matches = [('A', 'B'), ('E', 'F')]
-    confirmed_matches = [('C', 'D')]
-    deriver = CouplesDeriver(weekly_guesses, confirmed_matches, confirmed_no_matches)
-    assert(deriver.derive() == {"derived_matches": {('G', 'H'), ('I', 'J')}, "derived_no_matches": {('A', 'E'), ('B', 'F')}})
-
-
-#
-# def test_derives_matches_across_multiple_week_using_derived_information():
-#     weekly_guesses = [([('A', 'B'), ('C', 'D'), ('E', 'F'), ('G', 'H'), ('I', 'J')], 3),
-#                       ([('A', 'E'), ('C', 'D'), ('B', 'F'), ('G', 'J'), ('I', 'H')], 3)]
-#     confirmed_no_matches = [('A', 'B'), ('E', 'F')]
-#     confirmed_matches = [('C', 'D')]
-#     deriver = CouplesDeriver(weekly_guesses, confirmed_matches, confirmed_no_matches)
-#     assert(deriver.derive() == {"derived_matches": {('G', 'H'), ('I', 'J'), ('A', 'E'), ('B', 'F')}, "derived_no_matches": set()})
-#

077f0ff20108a60e1c9bae711e88e30c16966c9c
Rebecca Lelew <rebecca.lelew@springernature.com>
2020-04-26 15:06:13 +0100

    Add couples deriver

--- main/model/couples_deriver.py
+++ main/model/couples_deriver.py
@@ -9,19 +9,27 @@ class CouplesDeriver():
         if i >= len(self.weekly_guesses):
             return {"derived_no_matches": derived_no_matches, "derived_matches": derived_matches}
         (guess, no_correct) = self.weekly_guesses[i]
-        unknown_couples = set([couple for couple in guess if
-                               not couple_in_list(self.confirmed_matches, couple) and not couple_in_list(
-                                   self.confirmed_no_matches, couple)])
+        unknown_couples = set([couple for couple in guess if self.couple_not_in_confirmed_or_derived_lists(couple, derived_matches, derived_no_matches)])
+        print("unknown couples are {0}".format(unknown_couples))
         return self.derive(i + 1, derived_matches.union(self.derive_matches(guess, no_correct, unknown_couples)),
                            derived_no_matches.union(self.derive_no_matches(guess, no_correct, unknown_couples)))
 
+    def couple_not_in_confirmed_or_derived_lists(self, couple, derived_matches, derived_no_matches):
+        return not couple_in_list(self.confirmed_matches, couple) and \
+               not couple_in_list(self.confirmed_no_matches, couple) and \
+               not couple_in_list(derived_no_matches, couple) and \
+               not couple_in_list(derived_matches, couple)
+
     def derive_no_matches(self, guess, no_correct, unknown_couples):
-        unknown_couples_are_no_matches = number_of_couples_in_guess_from_list(guess, self.confirmed_matches) == no_correct
+        unknown_couples_are_no_matches = number_of_couples_in_guess_from_list(guess,
+                                                                              self.confirmed_matches) == no_correct
         return unknown_couples if unknown_couples_are_no_matches else set()
 
     def derive_matches(self, guess, no_correct, unknown_couples):
-        unknown_couples_are_matches = len(guess) - number_of_couples_in_guess_from_list(guess, self.confirmed_no_matches) == no_correct
+        unknown_couples_are_matches = len(guess) - number_of_couples_in_guess_from_list(guess,
+                                                                                        self.confirmed_no_matches) == no_correct
         return unknown_couples if unknown_couples_are_matches else set()
+
 
 def number_of_couples_in_guess_from_list(guess, list):
     return len([couple for couple in list if couple_in_list(guess, couple)])

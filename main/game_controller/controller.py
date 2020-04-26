821a79e8792ecb283a0f2dd3694c35a95ddb401a
Rebecca Lelew <rebecca.lelew@springernature.com>
2020-04-26 16:51:36 +0100

    Ensure a StraightController generates only male/female couples

--- main/game_controller/controller.py
+++ main/game_controller/controller.py
@@ -1,35 +1,44 @@
 import random
 from copy import deepcopy
+from abc import ABC, abstractmethod
 
 bisexual_contestants = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
-straight_contestants = bisexual_contestants + ['Q', 'R', 'S', 'T']
+straight_women = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+straight_men = ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
+straight_contestants = straight_women + straight_men
 
 
-class BaseController:
+class BaseController(ABC):
     def is_couple(self, couple):
         return couple in self.solution
 
     def number_correct_in_guess(self, guess):
         return len([couple for couple in guess if couple in self.solution])
 
+    @abstractmethod
+    def generate_solution(self):
+        pass
+
 
 class BisexualController(BaseController):
-    def __init__(self, solution=None):
+    def __init__(self, fixed_solution=None):
         self.contestants = deepcopy(bisexual_contestants)
-        self.solution = solution or generate_solution(self.contestants)
+        self.solution = fixed_solution or self.generate_solution(self.contestants)
+
+    def generate_solution(self):
+        random.shuffle(self.contestants)
+        return {_pair_at(i, contestants_to_shuffle) for i in range(0, len(contestants_to_shuffle) // 2)}
 
 
-class StraightController:
+class StraightController(BaseController):
     def __init__(self, solution=None):
         self.contestants = deepcopy(straight_contestants)
-        self.solution = solution or generate_solution(self.contestants)
+        self.solution = solution or self.generate_solution(self.contestants)
+
+    def generate_solution(self, contestants):
+        shuffled_men = random.shuffle(deepcopy(straight_men))
+        return {frozenset(couple) for couple in zip(straight_women, shuffled_men)}
 
 
-def generate_solution(contestants):
-    contestants_to_shuffle = deepcopy(contestants)
-    random.shuffle(contestants_to_shuffle)
-    return {pair_at(i, contestants_to_shuffle) for i in range(0, len(contestants_to_shuffle) // 2)}
-
-
-def pair_at(i, contestants):
-    return frozenset((contestants[2 * i], contestants[2 * i + 1]))
+def _pair_at(self, i, shuffled_contestants):
+    return frozenset((shuffled_contestants[2 * i], shuffled_contestants[2 * i + 1]))
--- test/unit/test_game_controller.py
+++ test/unit/test_game_controller.py
@@ -7,18 +7,18 @@ def test_generates_a_bisexual_game():
     assert len(generator.solution) == 8
 
 
-def test_generates_a_bisexual_game():
+def test_generates_a_straight_game():
     generator = StraightController()
-    assert solution_contains_all_contestants_only_once(generator.solution, straight_contestants)
+    assert solution_does_not_mix_sexes_for_straight_season(generator.solution)
     assert len(generator.solution) == 10
 
 
 def test_marks_a_correct_truth_booth():
-    assert generator_with_fixed_solution.is_couple({'B', 'A'})
+    assert bisexual_generator_with_fixed_solution.is_couple({'B', 'A'})
 
 
 def test_marks_an_incorrect_truth_booth():
-    assert not generator_with_fixed_solution.is_couple({'C', 'A'})
+    assert not bisexual_generator_with_fixed_solution.is_couple({'C', 'A'})
 
 
 def test_marks_a_match_up_ceremony():
@@ -30,11 +30,11 @@ def test_marks_a_match_up_ceremony():
              frozenset(('M', 'O')),
              frozenset(('J', 'L')),
              frozenset(('N', 'P'))}
-    assert (generator_with_fixed_solution.number_correct_in_guess(guess) == 4)
+    assert (bisexual_generator_with_fixed_solution.number_correct_in_guess(guess) == 4)
 
 
 def test_marks_a_correct_match_up_ceremony():
-    assert (generator_with_fixed_solution.number_correct_in_guess(mocked_solution) == 8)
+    assert (bisexual_generator_with_fixed_solution.number_correct_in_guess(mocked_solution) == 8)
 
 
 mocked_solution = {frozenset(('A', 'B')),
@@ -45,8 +45,28 @@ mocked_solution = {frozenset(('A', 'B')),
                    frozenset(('K', 'L')),
                    frozenset(('M', 'N')),
                    frozenset(('O', 'P'))}
-generator_with_fixed_solution = BisexualController(mocked_solution)
+bisexual_generator_with_fixed_solution = BisexualController(mocked_solution)
 
 
 def solution_contains_all_contestants_only_once(solution, expected_contestants):
     return sorted([contestant for pair in solution for contestant in pair]) == expected_contestants
+
+
+def solution_does_not_mix_sexes_for_straight_season(solution):
+    return all([is_heterosexual_pair(pair) for pair in solution])
+
+
+def is_heterosexual_pair(pair):
+    a, b = list(pair)
+    if is_woman(a):
+        return is_man(b)
+    else:
+        return is_woman(b)
+
+
+def is_man(person):
+    return person in straight_men
+
+
+def is_woman(person):
+    return person in straight_women

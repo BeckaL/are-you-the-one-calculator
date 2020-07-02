09dfafd7ad543b5cfaf550e693eeb7eb0decb16c
Rebecca Lelew <rebecca.lelew@springernature.com>
2020-07-02 20:30:52 +0100

    Add formatter for straight season

--- main/display/probability_formatter.py
+++ main/display/probability_formatter.py
@@ -6,21 +6,41 @@ class Formatter():
         self.women = women
         self.men = men
 
+    def longest_name_length(self):
+        return max([len(name) for name in self.women + self.men])
+
+    def top_left_space(self):
+        return " " * self.longest_name_length() + "  "
+
+    def pad_contestant_name_in_header(self, name):
+        min_number_length = 4
+        left_padding = math.floor((min_number_length - len(name))/2)
+        right_padding = math.ceil((min_number_length - len(name))/2)
+        end_space = "   "
+        return left_padding * " " + name + right_padding * " " + end_space
+
+    def pad_row_label(self, name):
+        padding_required = self.longest_name_length() - len(name)
+        return name + " " * padding_required
+
     def printable_grid(self):
-        top_row = ["   " + "      ".join(self.women)]
+        top_row = [self.top_left_space() + "".join([self.pad_contestant_name_in_header(c) for c in self.women])]
         probability_rows = [self.probabilities_for_woman(w) for w in self.women]
-        other_rows = [[self.men[i]] + probability_rows[i] for i in range(len(self.men))]
+        other_rows = [[self.pad_row_label(self.men[i])] + probability_rows[i] for i in
+                      range(len(self.men))]
         printable_other_rows = ["  ".join(row) for row in other_rows]
         rows = top_row + printable_other_rows
         return "\n".join(rows)
 
     def probabilities_for_woman(self, woman):
-        return list(map(lambda man: self.pad_digit(self.probabilities_hash[(woman, man)]), self.men))
+        return list(map(lambda man: self.pad_digit(self.probabilities_hash[(woman, man)], len(man)), self.men))
 
-    def pad_digit(self, n):
+    def pad_digit(self, n, length_of_name):
+        left_padding = math.floor((length_of_name - 4)/2) * " "
+        right_padding = math.ceil((length_of_name - 4)/2) * " "
         string_n = str(n)
         no_of_spaces = 5 - len(string_n)
-        return string_n + (" " * no_of_spaces)
+        return left_padding + string_n + (" " * no_of_spaces) + right_padding
 
 
 class BiFormatter():
